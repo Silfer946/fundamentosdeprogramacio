20c2d5ec23046f0d3682278494bc9fe08a8218fc
Silvestre <silvestreferfer@gmail.com>
2024-10-18 12:33:51 -0600

    04 Ejercicio 4

---
+++ ejercicio4.py
@@ -0,0 +1,26 @@
+"""
+Ejercicio 4
+Ddo los umeros, mostar la suma, resta, divicion 
+y multiplicacion
+Entarda:
+   numero1: int
+   numero2: int
+salida:
+   suma
+   resta
+   divicion 
+   multiplicacion 
+"""
+numero1 = input("Ingresa el primer numero:; ")
+numero1 = int(numero1)
+numero2 = input("Ingresa el segundo numero: ")
+numero2 = int(numero2)
+suma = numero1 + numero2
+resta =numero1 - numero2
+multiplicacion = numero1 * numero2 
+division = numero1 / numero2
+print("La suam es: ", suma)
+print("La resta es ", resta)
+print("La multiplicasion es: ", multiplicacion)
+print("La division es: ", division)
+
